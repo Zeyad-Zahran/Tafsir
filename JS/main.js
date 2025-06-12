6860a675009038d7831d17849799111b34451edd
Zeyad-Zahran <zeyadzahran56@gmail.com>
2025-06-12 22:17:08 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -280,7 +280,7 @@
             showLoading();
             
             try {
-                const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent', {
+                const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-8b:generateContent', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
